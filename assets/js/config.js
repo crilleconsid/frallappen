ed381d27383d4f7269f2b419ffb8c3d0d33e902a
crilleconsid <christopher.sornmo@consid.se>
2017-11-30 23:09:07 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyBNebgzr-MZH8K55utbCSbribgfYE6twJg",
-	authDomain: "frallapp-39aa4.firebaseapp.com",
-	databaseURL: "https://frallapp-39aa4.firebaseio.com",
-	projectId: "frallapp-39aa4",
-	storageBucket: "frallapp-39aa4.appspot.com",
-	messagingSenderId: "1077381395124"
+	apiKey: "YOUR CONFIG HERE",
+	authDomain: "YOUR CONFIG HER",
+	databaseURL: "YOUR CONFIG HER",
+	projectId: "YOUR CONFIG HER",
+	storageBucket: "YOUR CONFIG HER",
+	messagingSenderId: "YOUR CONFIG HER"
 };
 firebase.initializeApp(config);
